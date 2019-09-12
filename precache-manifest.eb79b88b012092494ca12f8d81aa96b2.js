03313aee53e610c70e1ba986a17678875fb4ab24
steve0079 <49959235+steve0079@users.noreply.github.com>
2019-09-12 13:29:50 -0700

    Updates

---
+++ precache-manifest.eb79b88b012092494ca12f8d81aa96b2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2dcca871a605a1d5e3660ddb59ed228c",
+    "url": "/github-api/index.html"
+  },
+  {
+    "revision": "f94b4a84369c0dfd69b3",
+    "url": "/github-api/static/css/main.716144b5.chunk.css"
+  },
+  {
+    "revision": "31be450ada38ca829e1b",
+    "url": "/github-api/static/js/2.94f30d6a.chunk.js"
+  },
+  {
+    "revision": "f94b4a84369c0dfd69b3",
+    "url": "/github-api/static/js/main.8e9268c2.chunk.js"
+  },
+  {
+    "revision": "2de8e9e0aa775333a9a0",
+    "url": "/github-api/static/js/runtime~main.cf28fc0c.js"
+  }
+]);
